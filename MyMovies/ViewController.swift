770b8afeb9bbeb9065a38f435314530d16d16977
Franco Ghiotti <fran.ghiott@gmail.com>
2021-06-06 12:58:31 +0200

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Franco Ghiotti on 6/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
